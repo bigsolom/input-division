fafa08400a5f1cef8f90a3f6acddd0faafb398e7
bigsolom <eslam.foad@gmail.com>
2023-05-11 16:07:58 +0200

    test structure

---
+++ src/test/java/de/neuefische/MainTest.java
@@ -0,0 +1,16 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void divide_whenGivenADenominatorOfZero_shouldnotThrowAnException() throws Exception{
+
+    }
+
+    @Test
+    public void divide_whenGivenTwoIntegers_shouldReturnDivisionResult() throws Exception{
+
+    }
+}
